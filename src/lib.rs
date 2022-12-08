78554e88b4e5363ef1463baa4d7509f750b567c4
daveiator <43887102+daveiator@users.noreply.github.com>
2022-12-08 22:36:22 +0100

    works with font scaling

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use eframe::{egui::{Widget, self, Ui}, epaint::Color32};
-#[derive(Debug, Default)]
+use eframe::{epaint, egui::{Widget, self, Ui}, epaint::Color32};
+
+#[derive(Debug)]
 pub struct DipSwitch {
     value: u16,
     size: u8,
@@ -13,33 +14,60 @@ impl DipSwitch {
         }
     }
 
-    fn draw_switch(&self, painter: &egui::Painter, rect: egui::Rect, value: bool, number: u8) {
+    fn draw_switch(&self, ui: &mut Ui, value: bool, index: u8) {
+        //Draws a switch with the given value
+        // ui.allocate_at_least(egui::vec2(10.0, 20.0), egui::Sense::click());
+        // allocate space for the switch
+        ui.allocate_space(egui::vec2(ui.style().text_styles.get(&egui::TextStyle::Body).unwrap().size * 10.0/14.0 , ui.style().text_styles.get(&egui::TextStyle::Body).unwrap().size * 20.0/14.0));
+        let rect = ui.min_rect();
+        let painter = ui.painter();
+
+
         let outer_switch = egui::Rect::from_min_size(rect.min + rect.size() * 0.1, rect.size() * 0.8);
         let rim_offset = egui::vec2(outer_switch.size().x *0.1, outer_switch.size().x *0.1);
         let inner_size = outer_switch.size() * egui::vec2(1.0, 0.5) - rim_offset * 2.0;
         let inner_switch = egui::Rect::from_min_size(outer_switch.min + rim_offset + egui::vec2(0.0, !value as u8 as f32 * inner_size.y+ rim_offset.y), inner_size);
         painter.rect_filled(outer_switch, 0.0, Color32::from_gray(27));
-        painter.rect_filled(inner_switch, 0.0, Color32::GRAY);
+        painter.rect_filled(inner_switch, 0.0, ui.visuals().widgets.inactive.fg_stroke.color);
+    }
+
+    fn draw_pre_label(&self, ui: &mut Ui) {
+        //Draws a label on the left side of the widget containing the label ON and an arrow pointing up
+        ui.vertical(|ui| {
+            ui.style_mut().spacing.item_spacing = egui::vec2(0.0, 0.0);
+            ui.add(egui::Label::new("ON").wrap(false));
+            ui.style_mut().text_styles.get_mut(&egui::TextStyle::Body).unwrap().size = 20.0/14.0 * ui.style().text_styles.get(&egui::TextStyle::Body).unwrap().size;
+            ui.add(egui::Label::new("⬆"));
+        });
     }
 }
 
 impl Widget for DipSwitch {
     fn ui(self, ui: &mut Ui) -> egui::Response {
-
-        let single_switch_size = egui::vec2(20.0*3.0, 40.0*3.0);
-
-        let (rect, response) = ui.allocate_exact_size(single_switch_size * egui::vec2((self.size as f32), 1.0), egui::Sense::click());
-        ui.painter().rect_filled(rect, 2.0, ui.visuals().widgets.inactive.bg_fill);
-        for i in 0..self.size {
-            let mut value = false;
-            if self.value & (1 << i) != 0 {
-                value = true;
-            }
-            //draw from left to right
-            let switch_rect = egui::Rect::from_min_size(rect.min + egui::vec2(single_switch_size.x * i as f32, 0.0), single_switch_size);
-            
-            self.draw_switch(ui.painter(), switch_rect, value, i);
-        }
-        response
+        ui.style_mut().visuals.widgets.noninteractive.fg_stroke.color = ui.style().visuals.widgets.inactive.fg_stroke.color;
+        return egui::containers::Frame::group(ui.style())
+        .inner_margin(2.0)
+        .rounding(0.0)
+        .fill(ui.visuals().widgets.inactive.bg_fill)
+        .stroke(egui::Stroke::none())
+        .show(ui, |ui| {
+            ui.shrink_height_to_current();
+            ui.shrink_width_to_current();
+            ui.horizontal_centered(|ui| {
+                ui.style_mut().spacing.item_spacing = egui::vec2(0.0, 0.0);
+                self.draw_pre_label(ui);
+                for i in 0..self.size {
+                    if i > 15 {
+                        return
+                    }
+                    let value = self.value & (1 << i) != 0;
+                    ui.vertical(|ui| {
+                        self.draw_switch(ui, value, i);
+                        ui.style_mut().spacing.item_spacing = egui::vec2(0.0, 0.0);
+                        ui.add(egui::Label::new((i+1).to_string()).wrap(false));
+                    });
+                }
+            })
+        }).response;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use eframe::{App, egui};
 use egui_dip_switch::DipSwitch;
 
-#[derive(Default)]
+
 struct SwitchTest {
-    value : u16
+    value : u16,
+    size: u8,
+    font_size: f32,
+}
+
+impl Default for SwitchTest {
+    fn default() -> Self {
+        Self {
+            value: 0,
+            size: 10,
+            font_size: 14.0,
+        }
+    }
 }
 
 impl App for SwitchTest {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
         eframe::egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Hello World");
-            ui.add(eframe::egui::Slider::new(&mut self.value, 0..=255).text("Value"));
-            ui.add(DipSwitch::new(8, self.value));
+            ui.add(eframe::egui::Slider::new(&mut self.font_size, 1.0..=100.0).text("Font Size"));
+            ui.add(eframe::egui::Slider::new(&mut self.size, 0..=10).text("Size"));
+            ui.add(eframe::egui::Slider::new(&mut self.value, 0..=u16::MAX).text("Value").step_by(1.0));
+            ui.style_mut().text_styles.get_mut(&egui::TextStyle::Body).unwrap().size = self.font_size;
+            ui.vertical(|ui| {
+                // ui.style_mut().text_styles.get_mut(&egui::TextStyle::Body).unwrap().size = 100.0;
+                ui.add(DipSwitch::new(self.size, self.value));
+            });
         });
     }
 }
